371b199565f68aa562f6050cfc9854bed769f8bd
Anthony Nixon <ajn0592@gmail.com>
2015-12-09 23:41:44 -0600

    Add LearnAbout mode.

--- roboreader.py
+++ roboreader.py
@@ -4,6 +4,8 @@
 import random
 import csv
 import json
+import wikipedia
+import re
 
 ################################################################################
 #                         Constant Definitions                                 #
@@ -12,13 +14,15 @@ punctuationMap = {
     ',': "<<COMMA>>",
     '.': "<<PERIOD>>",
     '!': '<<EXCLAMATION>>',
-    '?': '<<QUESTION>>'
+    '?': '<<QUESTION>>',
+    ':': '<<COLON>>'
     }
 finalizeMap = {
     '<<QUESTION>>': '?',
     '<<EXCLAMATION>>': '!',
     '<<PERIOD>>': '.',
-    '<<COMMA>>': ','
+    '<<COMMA>>': ',',
+    '<<COLON>>': ':'
     }
 
 ################################################################################
@@ -35,19 +39,20 @@ def loadDictionary():
     return dictionary
 
 def addWord(firstWord, secondWord):
-    firstWord = firstWord.lower()
-    secondWord = secondWord.lower()
-    firstWord.replace('"', '')
-    secondWord.replace('"', '')
+    firstWord = firstWord.lower().replace("\n", '').replace('.', '')
+    secondWord = secondWord.lower().replace("\n", '').replace('.', '')
+    firstWord.replace('"', ' ')
+    secondWord.replace('"', ' ')
 
-    if firstWord in dictionary:
-        if secondWord in dictionary[firstWord]:
-            dictionary[firstWord][secondWord] += 1
+    if firstWord != "" and secondWord != "":
+        if firstWord in dictionary:
+            if secondWord in dictionary[firstWord]:
+                dictionary[firstWord][secondWord] += 1
+            else:
+                dictionary[firstWord][secondWord] = 1
         else:
+            dictionary[firstWord] = {}
             dictionary[firstWord][secondWord] = 1
-    else:
-        dictionary[firstWord] = {}
-        dictionary[firstWord][secondWord] = 1
 
 def hasPunctuation(word):
     if word[-1:] in punctuationMap:
@@ -92,6 +97,7 @@ def chooseNextWord(word):
     return random.choice(weightedArray)
 
 def iterateInput(input):
+    input = input.replace("\n", ' ')
     words = input.split(" ")
     for i in range(0, (len(words)-1)):
         word = words[i]
@@ -119,25 +125,56 @@ def finalize(sentence):
     return sentence
 
 def makeSentence(word):
+    word = word.lower()
     def makeSentenceHelper(word, sentenceParts):
         if isFullStop(word):
             sentenceParts.append(word)
             return sentenceParts
         else:
             sentenceParts.append(word)
-            nextWord = chooseNextWord(word)
+            nextWord = chooseNextWord(word.lower())
             if (nextWord.lower() == '<<comma>>'):
                 sentenceParts.append('<<comma>>')
                 while (nextWord.lower() == '<<comma>>'):
                     nextWord = chooseNextWord(word)
             return makeSentenceHelper(nextWord, sentenceParts)
-    parts = makeSentenceHelper(word, [])
+    try:
+        parts = makeSentenceHelper(word, [])
+    except KeyError:
+        parts = makeSentenceHelper(word, [])
+
     sentence = ' '.join(parts)
 
 
     sentence = finalize(sentence)
 
     return sentence
+
+def crawlAndLearn(topic):
+    search = wikipedia.search(topic, results=5)
+    for page in search:
+        print "Learning about: " + page
+        try:
+            article = wikipedia.page(page)
+            content = re.sub(r'=+\sSee also\s=+.+$', ' ', article.content, flags=re.M | re.S)
+            content = re.sub(r'=+\s.+\s=+', ' ', content)
+            content = re.sub(r'\(.+\)', ' ', content, flags=re.M | re.S)
+            #print content
+            iterateInput(content)
+        except wikipedia.exceptions.DisambiguationError:
+            content = ""
+        except wikipedia.exceptions.PageError:
+            content = ""
+
+
+
+def learnAbout(topics):
+    try:
+        for topic in topics.split(): # string case
+            crawlAndLearn(topic)
+    except AttributeError:
+        for topic in topics:
+            crawlAndLearn(topic)
 
 
 
@@ -147,15 +184,18 @@ def makeSentence(word):
 ################################################################################
 #                         Run The Program                                      #
 ################################################################################
-dictionary = loadDictionary()
+# dictionary = loadDictionary()
+dictionary = {}
 
-testString = getFileContents('input.txt')
-iterateInput(testString)
+# testString = getFileContents('input.txt')
+# iterateInput(testString)
 
 # chooseNextWord('united')
 # chooseNextWord('the')
 # print chooseNextWord('has')
-print makeSentence('the')
+# print makeSentence('an')
+learnAbout('Minnesota')
+print makeSentence(random.choice(dictionary.keys()))
 
 
 saveDictionary(dictionary)
